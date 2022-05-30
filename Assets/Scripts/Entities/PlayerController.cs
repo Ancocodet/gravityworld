ad89a07f1e39c265d70bf916d97c8f0c3de7bd12
Ancocodet <info@ancozockt.de>
2022-05-30 21:21:49 +0200

    Update PlayerControlling

--- Assets/Scripts/Entities/PlayerController.cs
+++ Assets/Scripts/Entities/PlayerController.cs
@@ -7,9 +7,81 @@ public class PlayerController : MonoBehaviour
     
     public float xPosition = -7.5f;
     
+    private float gravityScale = 5.0f; 
+    private Rigidbody2D body;
+    
+    private bool canSwitch = false;
+    private ENextAction nextAction = ENextAction.NONE;
+    
     void Awake()
     {
+        body = GetComponent<Rigidbody2D>();
+        body.gravityScale = gravityScale;
+        
         transform.position = new Vector2(xPosition, (Camera.main.orthographicSize * -1f) + (1f + transform.localScale.y/2)); 
+    }
+    
+    void Update()
+    {
+        if(!canSwitch) return;
+        
+        if(nextAction != ENextAction.NONE)
+        {
+            bool moved = false;
+            
+            if(nextAction == ENextAction.UP  && body.gravityScale > 0f)
+            {
+                Debug.Log("Moving Up");
+                moved = true;
+                body.gravityScale = gravityScale * -1f;
+                body.rotation = 180f;
+            }
+            else if(nextAction == ENextAction.DOWN  && body.gravityScale < 0f)
+            {
+                Debug.Log("Moving Down");
+                moved = true;
+                body.gravityScale = gravityScale;
+                body.rotation = 0f;
+            }
+            else if(nextAction == ENextAction.SWITCH)
+            {
+                Debug.Log("Switching");
+                moved = true;
+                body.gravityScale *= -1f;
+                body.rotation += body.rotation == 0f ? 180f : 0f;
+            }
+            
+            nextAction = ENextAction.NONE;
+            if(moved)
+                canSwitch = false;
+        }
+    }
+    
+    public void SwitchGravity()
+    {
+        nextAction = ENextAction.SWITCH;
+    }
+    
+    public void MoveUp()
+    {
+        if(body.gravityScale > 0f)
+            nextAction = ENextAction.UP;
+    }
+    
+    public void MoveDown()
+    {
+        if(body.gravityScale < 0f)
+            nextAction = ENextAction.DOWN;
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Debug.Log("Collided");
+            if (!canSwitch)
+                canSwitch = true;
+        }
     }
     
 }
---
+++ Assets/Scripts/Scrolling/ObstacleBlock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ObstacleBlock : MonoBehaviour
+{
+    
+    public bool top = false;
+    public bool right = false;
+    
+    void Awake()
+    {
+        float x = right ? (Camera.main.orthographicSize * 2) + transform.localScale.x/2 : (Camera.main.orthographicSize * -2f) - transform.localScale.x/2;
+        float y = top ? Camera.main.orthographicSize - (transform.localScale.y/2) : (Camera.main.orthographicSize * -1f) + (transform.localScale.y/2);
+        transform.position = new Vector2(x, y);
+    }
+    
+}
